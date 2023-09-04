924ba15fb052e3951b7835d4721fefc4546ee4ab
TheTrishy <121170785+TheTrishy@users.noreply.github.com>
2023-09-04 19:45:44 -0400

    Create music-list.js

---
+++ music-list.js
@@ -0,0 +1,23 @@
+// Pra adicionar mais músicas, apenas copie o código e cole dentro do Array
+
+// {
+//  name: "Nome da música",
+//  artist: "Nome do artista",
+//  img: "nome da imagem aqui - deve estar em .jpg"
+//  src: "nome da música aqui - deve estar em formato .mp3"
+//}
+
+let allMusic = [
+    {
+        name: "Song of the Ancients - Popola",
+        artist: "Nier: Replicant",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name:"In Hell We Live - Lament",
+        artist:"Mili",
+        img:"music-2",
+        src:"music-2"
+    },
+];
